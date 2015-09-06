23c56b82ff04a459408ad994064f15cd13f709b5
eeue56 <eep41b@bangor.ac.uk>
2015-09-06 22:24:59 +0100

    rename things to make more sense

--- dumb-example/People.elm
+++ dumb-example/People.elm
@@ -1,25 +1,37 @@
 module People where
 
+import Dict exposing (Dict)
 import Set exposing (Set, toList, fromList)
+import Time exposing (every, second, Time)
 
 port newPerson : Signal String
 port isUnique : Signal Bool
 
-currentPeople = newPerson
+personId = every <| second
 
-updatePeople newName names =
-    names ++ [newName]
+addPerson : (String, Time) -> Dict Time String -> Dict Time String
+addPerson (newName, time) people =
+    Dict.insert time newName people
 
-names = Signal.foldp updatePeople [] currentPeople
+people' : Dict Time String
+people' = Dict.empty 
 
-port people : Signal (List String)
-port people = 
+people = Signal.foldp addPerson people'
+    <| 
+        Signal.map2 (\x y -> (x, y) ) newPerson <| Signal.sampleOn newPerson personId
+
+port names : Signal (List String)
+port names = 
     let 
-        people' : Bool ->  List String -> List String
-        people' isUnique names = 
-            if isUnique then 
-                (Set.toList << Set.fromList) names 
-            else names
+        names' : Bool ->  Dict Time String -> List String
+        names' isUnique people = 
+            let 
+                values = Dict.values people
+            in
+                if isUnique then 
+                    (Set.toList << Set.fromList) values 
+                else 
+                    values
     in
-        Signal.map2 (people') (isUnique) (names)
+        Signal.map2 (names') (isUnique) (people)
 
